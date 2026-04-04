61783913a9420047635cd0918266a4a934301b29
simontroyan <troyan.simon@gmail.com>
2026-04-04 18:58:30 +0200

    added arithmetic progression game

---
+++ brain_games/scripts/brain_arithm.py
@@ -0,0 +1,10 @@
+from brain_games.games.arithm_progression import ArithmeticProgression
+
+
+def main():
+    arithmeticProgression = ArithmeticProgression()
+    arithmeticProgression.run()
+    
+
+if __name__ == "__main__":
+    main()
