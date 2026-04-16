a7f8c9ab85333fc56e632a105746c341dfb2aa9e
simontroyan <troyan.simon@gmail.com>
2026-04-17 00:03:30 +0200

    added brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,9 @@
+import prompt
+
 def main():
     print("Welcome to the Brain Games!")
-
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
 
 if __name__ == "__main__":
     main()
