7246f39574e06e4b7957d3a81baeedc6b0e4cedf
simontroyan <troyan.simon@gmail.com>
2026-04-17 00:07:50 +0200

    added brain-games

--- brain_games/base_game.py
+++ brain_games/base_game.py
@@ -19,7 +19,9 @@ def run_game(description, generate_question_and_answer):
         else:
             print(
                 f'"{user_answer}" is wrong answer ;(. '
-                f'Correct answer was "{correct_answer}".'
+                f'Correct answer was "{correct_answer}".\n'
+                f"Let's try again, {user_name}!"
+
             )
             return
     print(f"Congratulations, {user_name}!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 import prompt
 
+
 def main():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
+
 
 if __name__ == "__main__":
     main()
